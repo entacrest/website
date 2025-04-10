db2e853d7dbcda755d3140941c15f105f3dee3ce
Abeeb Maroof <abeebdon@gmail.com>
2025-04-10 21:49:23 +0100

    no subscriber@example.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Entacrest Nexus",
+  title: "Entacrest",
   description: "Entacrest Webpage",
-  icons: {
-    icon: "/logo.png",
-  },
 };
 
 export default function RootLayout({
