abbdc1860d48c3b249f1e1449f05d80ad786be3f
Abeeb Maroof <abeebdon@gmail.com>
2025-04-24 17:38:20 +0100

    modifying image part

--- src/features/product/ProductPage.tsx
+++ src/features/product/ProductPage.tsx
@@ -23,7 +23,7 @@ const ProductPage = () => {
           </div>
           <div className="w-full md:basis-1/2 flex justify-center">
             <Image
-              src="/icons/product1.svg"
+              src="/icons/royalestate.jpg"
               alt="product"
               width={400}
               height={600}
