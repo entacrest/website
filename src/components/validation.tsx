4c4419ba33af0f49731a484df2303da7c71b6730
Abeeb Maroof <abeebdon@gmail.com>
2025-05-01 17:05:30 +0100

    fixes

--- src/components/validation.tsx
+++ src/components/validation.tsx
@@ -1,10 +1,23 @@
 import * as yup from "yup";
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 export const ContactFormSchema = yup.object().shape({
-  first_name: yup.string().required("First name is required"),
-  last_name: yup.string().required("Last name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
+  first_name: yup
+    .string()
+    .required("Name is required")
+    .matches(/^[A-Za-z\s]+$/, "Name can only contain letters "),
+  last_name: yup
+    .string()
+    .required("Name is required")
+    .matches(/^[A-Za-z\s]+$/, "Name can only contain letters "),
+  email: yup
+    .string()
+    .matches(emailRegex, "Invalid email format")
+    .required("Email is required"),
   phone: yup
     .string()
+    .matches(/^\d+$/, "Phone number must contain only digits")
+
     .matches(/^\d{10,15}$/, "Invalid phone number")
     .required("Phone number is required"),
   message: yup
@@ -13,9 +26,18 @@ export const ContactFormSchema = yup.object().shape({
     .min(10, "Message must be at least 10 characters"),
 });
 export const ApplicationFormSchema = yup.object().shape({
-  first_name: yup.string().required("First name is required"),
-  last_name: yup.string().required("Last name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
+  first_name: yup
+    .string()
+    .required("Name is required")
+    .matches(/^[A-Za-z\s]+$/, "Name can only contain letters "),
+  last_name: yup
+    .string()
+    .required("Name is required")
+    .matches(/^[A-Za-z\s]+$/, "Name can only contain letters "),
+  email: yup
+    .string()
+    .matches(emailRegex, "Invalid email format")
+    .required("Email is required"),
   role: yup.string().required("Enter the role you are applying for"),
   cover_letter: yup.string(),
 });
